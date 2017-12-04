54fbeb6e94a0a40f08648f0b8cf198d240d0ca3f
kfkrab <kfkrab@gmail.com>
2017-12-04 12:20:05 +0100

    a

--- index.php
+++ index.php
@@ -3,16 +3,38 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php bloginfo( 'title' ) ?></title>
+	<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" media="all">
 </head>
 <body>
 
-<h1>My fancy krabqe ladida theme</h1>
+<h1></h1>
 
+
+
+	
+	
+	
+	
+	
 	<header>
 	<div>
-	<?php
-		wp_page_menu()
-	?>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header"><a class="navbar-brand" href="#"><img src="assets/img/logo.png"></a>
+                    
+                </div>
+                
+                
+                <div class="collapse navbar-collapse" id="navcol-1">
+                   <?php
+						wp_page_menu()
+					?>
+                </div>
+            </div>
+        </nav>
+    </div>
+	<div>
+	
 	</div>
 	</header>
 
--- page-sticky.php
+++ page-sticky.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * Template Name: Side med priser
  */
 ?>
